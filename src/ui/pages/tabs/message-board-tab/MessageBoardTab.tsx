83b80cb15da2de0dcc45b1557e5ce21139206625
JonasRex <jonas.hansen@meew.dk>
2023-01-24 13:10:53 +0100

    Update MessageBoardTab.tsx

--- src/ui/pages/tabs/message-board-tab/MessageBoardTab.tsx
+++ src/ui/pages/tabs/message-board-tab/MessageBoardTab.tsx
@@ -1,42 +1,38 @@
-import React from 'react';
-import {
-  IonButton,
-  IonContent,
-  IonHeader,
-  IonItem,
-  IonTextarea,
-
-} from '@ionic/react';
+import { useState } from 'react';
+import { IonButton, IonContent } from '@ionic/react';
 
 import PostCard from 'ui/components/custom/PostCard';
 import MenuToolBar from 'ui/components/custom/MenuToolBar';
 
-const MessageBoardTab: React.FC = () => (
-  <>
-    
+type Post = {
+  id: number;
+  name: string;
+  date: string;
+  message: string;
+  likedByUser: boolean;
+  highFivedByUser: boolean;
+};
+
+const MessageBoardTab: React.FC = () => {
+  const [posts, setPosts] = useState<Post[] | null>([
+    { id: 0, name: 'Michael Jordan', date: 'Today', message: 'Test test test?', likedByUser: false, highFivedByUser: false },
+    { id: 1, name: 'Charles Barkley', date: 'Today', message: 'Test test test?', likedByUser: false, highFivedByUser: false },
+    { id: 2, name: 'Scottie Pippen', date: 'Today', message: 'Test test test', likedByUser: false, highFivedByUser: false },
+  ]);
+
+  return (
+    <>
       <MenuToolBar title="Forum" post={true} />
-      
-    
-    <IonContent id="message-board-content" color={'white-background'}>
-      <div className="px-5 pb-40">
-        <PostCard
-          name="Michael Jordan"
-          date="Today"
-          message="Lorem ipsum dolor sit amet consectetur adipisicing elit. Eos veritatis error fugit, reprehenderit tempora sed nisi consequatur laborum asperiores, natus facere quod quas veniam cupiditate non dicta illo, quos quidem?"
-        />
-        <PostCard
-          name="Charles Barkley"
-          date="Today"
-          message="Lorem ipsum dolor sit amet consectetur adipisicing elit. Eveniet hic veniam obcaecati libero quod nulla architecto reprehenderit facere officiis tenetur vitae tempora consectetur fugiat atque nam, voluptatum quaerat natus aspernatur. Labore quibusdam sapiente in dolor libero quaerat, sint repudiandae vel!"
-        />
-        <PostCard
-          name="Scottie Pippen"
-          date="Today"
-          message="Lorem ipsum dolor sit amet consectetur adipisicing elit. Illum delectus tenetur voluptas debitis."
-        />
-      </div>
-    </IonContent>
-  </>
-);
+
+      <IonContent id="message-board-content" color={'white-background'}>
+        <div className="px-5 pb-40">
+          {posts?.map((p, i) => {
+            return <PostCard key={i} name={p.name} date={p.date} message={p.message} />;
+          })}
+        </div>
+      </IonContent>
+    </>
+  );
+};
 
 export default MessageBoardTab;
