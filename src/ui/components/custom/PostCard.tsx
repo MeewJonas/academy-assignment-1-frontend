bc5ee527031f473029ef6c1bd69da7c4b23842e8
JonasRex <jonas.hansen@meew.dk>
2023-01-24 18:03:14 +0100

    delete posts

--- src/ui/components/custom/PostCard.tsx
+++ src/ui/components/custom/PostCard.tsx
@@ -1,39 +1,50 @@
 import { IonButton, IonCard, IonCardContent, IonCardHeader, IonCardSubtitle, IonCardTitle, IonIcon } from '@ionic/react';
 import { useState } from 'react';
-import { heartOutline, handRightOutline, chatboxEllipsesOutline } from 'ionicons/icons';
+import { heartOutline, handRightOutline, chatboxEllipsesOutline, closeOutline } from 'ionicons/icons';
 import { heart, handRight } from 'ionicons/icons';
-
+import { useAuthUserStore } from 'store/user';
 
 type PostCardProps = {
-  name: string;
-  date: string;
-  message: string;
+  post: {
+    id: string;
+    user: string;
+    userId: string;
+    date: string;
+    message: string;
+  };
+  onDelete: (id: string) => void;
 };
 
-const PostCard: React.FC<PostCardProps> = ({
-  name = 'John Doe',
-  date = '2023-01-01',
-  message = 'Bla Bla',
-}) => {
-
+const PostCard: React.FC<PostCardProps> = ({ post: { id, user, userId, date, message }, onDelete }) => {
+  const authUser = useAuthUserStore((state) => state.authUser);
   const [like, setLike] = useState(false);
   const [highFive, setHighFive] = useState(false);
-  
+
+
   return (
     <IonCard>
       <IonCardHeader>
-        <IonCardTitle>{name}</IonCardTitle>
+        <IonCardTitle>{user}</IonCardTitle>
 
         <IonCardSubtitle>{date}</IonCardSubtitle>
       </IonCardHeader>
 
       <IonCardContent>{message}</IonCardContent>
 
-      <IonButton onClick={()=> setLike(!like)} fill="clear">{!like ? <IonIcon icon={heartOutline} /> : <IonIcon className="fill-red-900" icon={heart} />}</IonButton>
-      <IonButton onClick={()=> setHighFive(!highFive)} fill="clear">{!highFive ? <IonIcon icon={handRightOutline} /> : <IonIcon className="fill-blue-900" icon={handRight} />}</IonButton>
+      <IonButton onClick={() => setLike(!like)} fill="clear">
+        {!like ? <IonIcon icon={heartOutline} /> : <IonIcon className="fill-red-900" icon={heart} />}
+      </IonButton>
+      <IonButton onClick={() => setHighFive(!highFive)} fill="clear">
+        {!highFive ? <IonIcon icon={handRightOutline} /> : <IonIcon className="fill-blue-900" icon={handRight} />}
+      </IonButton>
       <IonButton fill="clear">
         <IonIcon icon={chatboxEllipsesOutline} />
       </IonButton>
+      {authUser?.id === userId && (
+        <IonButton onClick={() => onDelete(id)} fill="clear">
+          <IonIcon icon={closeOutline} />
+        </IonButton>
+      )}
     </IonCard>
   );
 };
--- src/ui/pages/tabs/message-board-tab/MessageBoardTab.tsx
+++ src/ui/pages/tabs/message-board-tab/MessageBoardTab.tsx
@@ -9,6 +9,7 @@ import MenuToolBar from 'ui/components/custom/MenuToolBar';
 type Post = {
   id: string;
   user: string;
+  userId: string;
   date: string;
   message: string;
   //likedByUser: boolean;
@@ -25,7 +26,9 @@ const MessageBoardTab: React.FC = () => {
       //TODO - add range to select like this:   .range(0,9)
       const { data } = await supabase.from('post').select('*, profile(first_name, last_name)').order('created', { ascending: false });
       if (data === null) return;
-      setPosts(data.map((p) => ({ id: p.id, user: p.profile.first_name + ' ' + p.profile.last_name, date: p.created, message: p.message })));
+      setPosts(
+        data.map((p) => ({ id: p.id, user: p.profile.first_name + ' ' + p.profile.last_name, userId: p.profile_fk, date: p.created, message: p.message }))
+      );
     };
 
     fetchPosts();
@@ -34,14 +37,25 @@ const MessageBoardTab: React.FC = () => {
   const handleSend = async () => {
     if (newPost === '') return;
     const { data } = await supabase.from('post').insert({ message: newPost, profile_fk: authUser?.id }).select('*, profile(first_name, last_name)');
-    //const { data } = await supabase.from('post').select('*').eq('id', authUser?.id).single();
 
     if (data === null) return;
     setPosts([
-      { id: data[0].id, user: data[0].profile.first_name + ' ' + data[0].profile.last_name, date: data[0].created, message: data[0].message },
+      {
+        id: data[0].id,
+        user: data[0].profile.first_name + ' ' + data[0].profile.last_name,
+        userId: data[0].profile_fk,
+        date: data[0].created,
+        message: data[0].message,
+      },
       ...posts,
     ]);
     setNewPost('');
+  };
+
+  const handleDelete = async (id: string) => {
+    const { data } = await supabase.from('post').delete().match({ id: id });
+    if (data === null) return; 
+    setPosts(posts.filter((p) => p.id !== id));
   };
 
   return (
@@ -59,7 +73,7 @@ const MessageBoardTab: React.FC = () => {
       <IonContent id="message-board-content" color={'white-background'}>
         <div className="px-5 pb-40">
           {posts?.map((p, i) => {
-            return <PostCard key={i} name={p.user} date={p.date} message={p.message} />;
+            return <PostCard key={i} post={p} onDelete={handleDelete} />;
           })}
         </div>
       </IonContent>
