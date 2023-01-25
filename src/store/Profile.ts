180e86cc58ce568ea8d9a18ab6b799589930548e
JonasRex <jonas.hansen@meew.dk>
2023-01-25 10:40:38 +0100

    Update Profile.ts

--- src/store/Profile.ts
+++ src/store/Profile.ts
@@ -1,29 +1,37 @@
 import create from 'zustand';
-//import { Database } from 'types/database.types';
+import { supabase } from 'apis/supabaseClient';
+import { Database } from 'types/database.types';
 
-//type Profile = Database['public']['Tables']['profile']['Row'];
+type Profile = Database['public']['Tables']['profile']['Row'];
 
 interface ProfileState {
   //userProfile: Profile | null;
   firstName: string | null;
   lastName: string | null;
   age: number | null;
+  profiles: Profile[] | null;
   //setUserProfile: (userProfile: Profile) => void;
   setFirstName: (firstName: string) => void;
   setLastName: (lastName: string) => void;
   setAge: (age: number) => void;
   setUserProfile: (firstName: string, lastName: string, age: number) => void;
   resetUserProfile: () => void;
+  getAllUserProfiles: () => void;
 }
 
 export const useProfileStore = create<ProfileState>((set) => ({
   firstName: null,
   lastName: null,
   age: null,
+  profiles: [],
   //setUserProfile: (userProfile) => set({ userProfile  }),
   setFirstName: (firstName) => set({ firstName }),
   setLastName: (lastName) => set({ lastName }),
   setAge: (age) => set({ age }),
   setUserProfile: (firstName, lastName, age) => set({ firstName, lastName, age }),
   resetUserProfile: () => set({ firstName: null, lastName: null, age: null }),
+  getAllUserProfiles: async () => {
+    const { data } = await supabase.from('profile').select();
+    set({ profiles: data });
+  },
 }));
