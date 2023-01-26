2839a487b8c6b384b4628cc708dc0b402604f667
JonasRex <jonas.hansen@meew.dk>
2023-01-26 12:10:15 +0100

    Update DirectMessageTab.tsx
    Edge Functions

--- src/ui/pages/tabs/direct-message-tab/DirectMessageTab.tsx
+++ src/ui/pages/tabs/direct-message-tab/DirectMessageTab.tsx
@@ -1,12 +1,45 @@
-import React from 'react';
-import {  IonContent } from '@ionic/react';
+import { useState } from 'react';
+import { IonButton, IonContent, IonHeader, IonItem, IonTextarea } from '@ionic/react';
 import MenuToolBar from 'ui/components/custom/MenuToolBar';
+import { supabase } from 'apis/supabaseClient';
 
-const DirectMessageTab: React.FC = () => (
-  <>
-    <MenuToolBar title="Direct Message"  />
-    <IonContent color={'white-background'}></IonContent>
-  </>
-);
+const DirectMessageTab: React.FC = () => {
+  const [message, setMessage] = useState<string>('');
+  const [messages, setMessages] = useState<any[]>([{ sender: 'The Edge', message: 'What is your name?' }]);
+
+  const handleSend = async () => {
+    if (message === null) return;
+    const { data } = await supabase.functions.invoke('test', {
+      body: {
+        name: message,
+      },
+    });
+
+    if (data === null) return;
+    setMessages([...messages, { sender: 'Me', message: message }, { sender: 'The Edge', message: data.message }]);
+   
+  };
+  return (
+    <>
+      <MenuToolBar title="Direct Message" />
+      <IonHeader>
+        <IonItem className="border-solid border-4" lines="none">
+          <IonTextarea onIonChange={(e: any) => setMessage(e.detail.value)} value={message} placeholder="Skriv nyt opslag"></IonTextarea>
+          <IonButton onClick={handleSend} className="my-auto" slot="end" size="default">
+            Send
+          </IonButton>
+        </IonItem>
+      </IonHeader>
+      <IonContent color={'white-background'}>
+        {messages.map((m, i) => (
+          <IonItem key={i} className="border-solid border-4" lines="none">
+            {m.sender === 'The Edge' ? <h1 className="mr-1 text-blue-800">{m.sender}:</h1> : <h1 className="mr-1 text-red-800">{m.sender}:</h1>}
+            <p>{m.message}</p>
+          </IonItem>
+        ))}
+      </IonContent>
+    </>
+  );
+};
 
 export default DirectMessageTab;
